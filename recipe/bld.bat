6104b36c85212f32ed12c33521e46978530609a3
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-09-05 18:03:02 +0200

    Remove the --prefix argument to setup.py install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="" .. || exit /b
+cmake -G "%CMAKE_GENERATOR%" .. || exit /b
 cmake --build . --config Release || exit /b
 ctest -C Release || exit /b
 cmake --build . --config Release --target install || exit /b
