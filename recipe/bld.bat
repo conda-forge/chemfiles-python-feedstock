e09ed6e799233f4fa5622f1e06be4259649ad025
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-12-18 15:59:56 +0000

    Update recipe for incoming 0.8 version

--- recipe/bld.bat
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-
-cmake -G "%CMAKE_GENERATOR%" .. || exit /b
-cmake --build . --config Release || exit /b
-ctest -C Release || exit /b
-cmake --build . --config Release --target install || exit /b
--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-set -eu
-
-mkdir build
-cd build
-
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make
-ctest
-make install
